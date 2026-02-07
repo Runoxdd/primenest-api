7e3b2e9145546b690aac50a6d3c2fd303c7fdf8c
Runoxdd <runoefekemo@gmail.com>
2026-02-07 17:14:11 +0100

    fix: update cors to allow both local and production

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const app = express();
 
 // Use a simple, clean CORS configuration
 app.use(cors({ 
- origin: process.env.CLIENT_URL, // This will now be your Vercel link from Render Env
+ origin: [
+    "https://primenest-client.vercel.app", 
+    "http://localhost:5173"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
